922b644dc924e6e3b8208e64dcb8cde1a948592a
Mikhail Vialov <mikhail_vialov@epam.com>
2023-10-31 16:29:54 +0100

    sns adaptation

--- product-service/src/handlers/catalogBatchProcess.ts
+++ product-service/src/handlers/catalogBatchProcess.ts
@@ -6,13 +6,18 @@ import type {
 import { v4 as uuid } from "uuid";
 import { marshall } from "@aws-sdk/util-dynamodb";
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { SNSClient, PublishCommand } from '@aws-sdk/client-sns';
 
 const dynamoDB = new DynamoDB();
+const sns = new SNSClient({});
 
 export const handler: Handler = async (
   event: SQSEvent,
   _context: Context
 ): Promise<void> => {
+  const products = [];
+  const stocks = [];
+
   for (let record of event.Records) {
     const product = JSON.parse(record.body);
 
@@ -31,10 +36,20 @@ export const handler: Handler = async (
     await dynamoDB.putItem({
       TableName: process.env.PRODUCTS_TABLE_NAME,
       Item: marshall(productItem),
-    })
+    });
     await dynamoDB.putItem({
       TableName: process.env.STOCKS_TABLE_NAME,
       Item: marshall(stockItem),
-    })
+    });
+    products.push(productItem);
+    stocks.push(stockItem);
   }
+
+  const message = `Created products: ${JSON.stringify(products)}\nCreated stocks: ${JSON.stringify(stocks)}`;
+  const command = new PublishCommand({
+    Subject: 'New products ware created',
+    Message: message,
+    TopicArn: process.env.SNS_ARN
+  });
+  await sns.send(command);
 };
